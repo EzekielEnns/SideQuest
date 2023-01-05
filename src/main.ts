d47aa57c995d1e5b91d3051597bc8ae8fa2d162d
Ezekiel Enns <ezekielenns@pop-os.localdomain>
2023-01-04 23:46:45 -0400

    cleaned a bit

--- src/main.ts
+++ src/main.ts
@@ -6,32 +6,103 @@ import {PointerLockControls} from 'three/examples/jsm/controls/PointerLockContro
 
 import('@dimforge/rapier3d').then(R=> {
 
+//BOILERPLATE
 const width = window.innerWidth;
 const height = window.innerHeight;
-const clock = new THREE.Clock();
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 60, width / height, 0.01, 100 );
+const camera = new THREE.PerspectiveCamera( 60, width / height, 0.01, 100 ); 
 camera.position.y = 1;
-
 //render setup
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( document.body.clientWidth, window.innerHeight );
 document.body.appendChild(renderer.domElement);
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.BasicShadowMap;
+window.addEventListener('resize', function() {
+    var width = document.body.clientWidth;
+    var height = window.innerHeight;
+    renderer.setSize( width, height );
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+});
 
-let prevTime = performance.now();
-
-//controls 
-const velocity = new THREE.Vector3();
+///controls
 const controls = new PointerLockControls(camera,document.body);
+scene.add(controls.getObject())
+document.body.addEventListener('click',function () {controls.lock()})
+
+
+///meshes
+const cSize = 2;
+const mesh = new THREE.Mesh(
+	new THREE.BoxGeometry( cSize, cSize, cSize ),
+	new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: false} )
+);
+mesh.position.z = -10;
+mesh.position.y = 5;
+scene.add( mesh );
+const gridHelper = new THREE.GridHelper( 100, 100 );
+gridHelper.position.y = 0;
+scene.add( gridHelper );
+
+//physics 
+//TODO rename vars
+let gravity = {x:0.0, y:-9.81, z:0.0}
+let world = new R.World(gravity)
+let groundColDesc = R.ColliderDesc.cuboid(50.0, 0.1, 50.0)
+                      .setFriction(0.6)
+world.createCollider(groundColDesc)
+let ridgeBodydesc = R.RigidBodyDesc.dynamic()
+            .setTranslation(mesh.position.x,mesh.position.y,mesh.position.z)
+            .setAdditionalMass(100.0)
+let ridgeBody = world.createRigidBody(ridgeBodydesc);
+let colDesc = R.ColliderDesc.cuboid(cSize/2, cSize/2, cSize/2)
+let test = world.createCollider(colDesc,ridgeBody)
+ridgeBody.resetForces(true);
+let playerBody = R.RigidBodyDesc.dynamic()
+                    .setLinearDamping(11.0)
+                    .setAngularDamping(11.0)
+                    .setTranslation(camera.position.x, camera.position.y, camera.position.z)
+                    .setAdditionalMass(100.0)
+let playerCol = R.ColliderDesc.cuboid(1, 1, 1)
+let player = world.createRigidBody(playerBody)
+let plr = world.createCollider(playerCol,player)
+
+//Shooting
+//world Vector 
+const dir = new Vector3(0.0,0.0,0.0)
+let rayD = {
+        x:camera.getWorldDirection(dir).x,
+        y:camera.getWorldDirection(dir).y,
+        z:camera.getWorldDirection(dir).z
+}
+let hit:unknown;
+document.addEventListener('mousedown', function(e){
+    //TODO set dir to player look
+    //sets ray center of screen 
+    //TODO add is lock option
+    rayD = {
+            x:camera.getWorldDirection(dir).x,
+            y:camera.getWorldDirection(dir).y,
+            z:camera.getWorldDirection(dir).z
+    }
+    let {x:Px,y:Py,z:Pz} = camera.position
+    //Next thing to take on shoots fired 
+    let ray = new R.Ray({x:Px,y:Py,z:Pz},{...rayD})
+    let hit = plr.castRay(ray, 200, true);
+    if(hit == ridgeBody.handle){
+        console.log("shots fired")
+        if (test.parent()){
+        test.parent().applyImpulse(
+                {x:0.0,y:500.0,z:0.0},
+        true)
+        }
+    }
+})
 let moveForward = false;
 let moveBackward = false;
 let moveLeft = false;
 let moveRight = false;
-let jump= false;
-document.body.addEventListener('click',function () {controls.lock()})
-scene.add(controls.getObject())
 document.addEventListener('keydown', function ({code}){
     switch ( code ) {
         case 'ArrowUp':
@@ -53,9 +124,8 @@ document.addEventListener('keydown', function ({code}){
         case 'KeyD':
             moveRight = true;
             break;
-
         case 'Space':
-            jump = true;
+            //TODO cast
             break;
 
     }
@@ -86,184 +156,35 @@ document.addEventListener('keyup', function({code}){
     }
 });
 
-// Resize to the size of the screen
-window.addEventListener('resize', function() {
-    var width = document.body.clientWidth;
-    var height = window.innerHeight;
-    renderer.setSize( width, height );
-    camera.aspect = width / height;
-    camera.updateProjectionMatrix();
-
-});
-
-//world 
-const cSize = 2;
-const mesh = new THREE.Mesh(
-	new THREE.BoxGeometry( cSize, cSize, cSize ),
-	new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: false} )
-);
-mesh.position.z = -10;
-mesh.position.y = 5;
-scene.add( mesh );
-const gridHelper = new THREE.GridHelper( 100, 100 );
-gridHelper.position.y = 0;
-scene.add( gridHelper );
-
-//physics 
-let gravity = {x:0.0, y:-9.81, z:0.0}
-let world = new R.World(gravity)
-let groundColDesc = R.ColliderDesc.cuboid(50.0, 0.1, 50.0)
-                      .setFriction(0.6)
-world.createCollider(groundColDesc)
-
-let ridgeBodydesc = R.RigidBodyDesc.dynamic()
-            .setTranslation(mesh.position.x,mesh.position.y,mesh.position.z)
-            .setAdditionalMass(100.0)
-let ridgeBody = world.createRigidBody(ridgeBodydesc);
-let colDesc = R.ColliderDesc.cuboid(cSize/2, cSize/2, cSize/2)
-let test = world.createCollider(colDesc,ridgeBody)
-ridgeBody.resetForces(true);
-
-
-//world Vector 
-const dir = new Vector3(0.0,0.0,0.0)
-let ray:Ray;
-let rayD = {
-        x:camera.getWorldDirection(dir).x,
-        y:camera.getWorldDirection(dir).y,
-        z:camera.getWorldDirection(dir).z
-}
-
-let playerBody = R.RigidBodyDesc.dynamic()
-                    .setLinearDamping(11.0)
-                    .setAngularDamping(11.0)
-                    .setTranslation(camera.position.x, camera.position.y, camera.position.z)
-                    .setAdditionalMass(100.0)
-let playerCol = R.ColliderDesc.cuboid(1, 1, 1)
-let player = world.createRigidBody(playerBody)
-let plr = world.createCollider(playerCol,player)
-
-let hit:unknown;
-
-//raycasting 
-document.addEventListener('mousedown', function(e){
-    //sets ray center of screen 
-    rayD = {
-            x:camera.getWorldDirection(dir).x,
-            y:camera.getWorldDirection(dir).y,
-            z:camera.getWorldDirection(dir).z
-    }
-    let {x:Px,y:Py,z:Pz} = camera.position
-    //Next thing to take on shoots fired 
-    ray = new R.Ray({x:Px,y:Py,z:Pz},{...rayD})
-
-})
-
 //inital render 
 renderer.render( scene, camera );
 
 ( function anim () {
 	requestAnimationFrame( anim );
-    world.step()//move physics 
 
-    const time = performance.now();
-    //movment 
+    world.step()//keep out of is locked TODO find out 
     if(controls.isLocked){
-        const moveSpeed = 10.0;
-        const jumpheight = 150.0;
-       //note grid is reverse 
+       //TODO move this to movemnt function
+       const moveSpeed = 10.0;
        let rotate = controls.getObject().rotation;
        const direction = new THREE.Vector3();
+       //TODO clean up this math
        direction.z = -1*(Number( moveForward ) - Number( moveBackward ))*moveSpeed;
        direction.x = (Number( moveRight ) - Number( moveLeft ))*moveSpeed;
        direction.applyEuler(rotate)
-       direction.multiplyScalar(moveSpeed) //movement speed 
-    
-       //so apply impulse actually results in a floaty effect 
-       //not really good for clear fine movment 
-       //but jumping feels amazing 
+       direction.multiplyScalar(moveSpeed) 
        player.applyImpulse({x:direction.x,y:0.0,z:direction.z }, true)
+
+       //aligning bodies TODO abstract this
        let rlPos = player.translation();
        controls.getObject().position.set(rlPos.x,rlPos.y,rlPos.z)
-        //shots fired 
-        //note this dose not work initally 
-        if(ray){
-            let hit = plr.castRay(ray, 200, true);
-            console.log(`what i got:: ${hit}, ${test.handle} ${ridgeBody.handle}`)
-            if(hit == ridgeBody.handle){
-                console.log("shots fired")
-                if (test.parent()){
-                test.parent().applyImpulse(
-                        {x:0.0,y:500.0,z:0.0},
-                true)
-                }
-            }
-            ray = undefined;
-        }
+       let newPos = ridgeBody.translation();
+       mesh.position.set(newPos.x,newPos.y,newPos.z);
     }
 
 
-
-    //physics 
-    let newPos = ridgeBody.translation();
-    mesh.position.set(newPos.x,newPos.y,newPos.z);
     
-    prevTime = time;
     renderer.render( scene, camera );
 } )();
 
 })
-/*
-    function onKeyDown({keyCode} :{keyCode:number}) {
-        switch (keyCode){
-            case 38:
-            case 87:
-                moveForward = true;
-                console.log("onKeyDown! moveForward is now: " + moveForward)
-                break;
-        }
-    }
-    function onKeyUp({keyCode} :{keyCode:number}) {
-        switch (keyCode){
-            case 38:
-            case 87:
-                moveForward = false;
-                console.log("onKeyup! moveForward is now: " + moveForward)
-                break;
-        }
-    }
-    window.addEventListener("keydown",({key})=>{
-        switch(key) {
-            case "w":
-            case "ArrowUp":
-                camera.position.addScaledVector(y, movment)
-                console.log('up')
-                break;
-            case "s":
-            case "ArrowDown":
-                camera.position.addScaledVector(y, -1*movment)
-                console.log('Down')
-                break;
-            case "d":
-            case "ArrowRight":
-                camera.position.addScaledVector(x, movment)
-                console.log('right')
-                break;
-            case "a":
-            case "ArrowLeft":
-                camera.position.addScaledVector(x, -1*movment)
-                console.log('left')
-                break;
-        }
-        renderer.render(scene, camera);
-    } )
-    */
-    /*
-    so all objectes auto update there scene
-    you can access their info via the geometry property
-
-    the projection matrix needs to be chnaged each time you move 
-    so something like 
-        camera.aspect = window.innerWidth/window.innerHeight
-        camera.updateProjectionMatrix();
-*/
